d09f6c15a36a7c48839968b538085ab5ecd2141a
mikel <mikelgoti@gmail.com>
2023-01-12 12:15:05 +0100

    segundo commit

---
+++ iptables/prueba.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "hola escribe tu nombre: "
+read nombre
+
+echo "tu nombre es $nombre"
--- iptables/script_ip_tables.sh
+++ iptables/script_ip_tables.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+echo "Introduce la interfaz para aplicar las reglas del script: "
+read i
+
 #Eliminar posibles reglas existentes
 iptables -t filter -F
 iptables -t nat -F
 
 #Reiniciar contadores
-iptables -t filer -Z
+iptables -t filter -Z
 iptables -t nat -Z
 
 #DENEGAR TODO
@@ -18,26 +21,25 @@ iptables -I INPUT 1 -i lo -j ACCEPT
 iptables -I OUTPUT 1 -o lo -j ACCEPT
 
 #ENABLE PING
-iptables -A OUTPUT -o [interface name] -p icmp -j ACCEPT
-iptables -A INPUT -i [interface name] -p icmp -j ACCEPT
+iptables -A OUTPUT -o $i -p icmp -j ACCEPT
+iptables -A INPUT -i $i -p icmp -j ACCEPT
 
 #ENABLE DNS
 #PROTOCOLO UPD
-iptables -A OUTPUT -o [interface name] -p udp --dport 53 -j ACCEPT
-iptables -A INPUT -i [interface name] -p udp --sport 53 -j ACCEPT
+iptables -A OUTPUT -o $i -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -i $i -p udp --sport 53 -j ACCEPT
 
 #PROTOCOLO TCP
-iptables -A OUTPUT -o [interfcae name] -p tcp --dport 53 -j ACCEPT
-iptables -A INPUT -i [interface name] -p tcp --sport 53 -j ACCEPT
+iptables -A OUTPUT -o $i -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -i $i -p tcp --sport 53 -j ACCEPT
 
 #HTTP
-iptables -A OUTPUT -o [interface name] -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -i [interface name] -p tcp --sport 80 -j ACCEPT
+iptables -A OUTPUT -o $i -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -i $i -p tcp --sport 80 -j ACCEPT
 
 #HTTPS
-iptables -A OUTPUT -o [interface name] -p tcp --dport 443 -j ACCEPT
-iptables -A INPUT -i [interface name ] -p tcp --sport 443 -j ACCEPT
-
+iptables -A OUTPUT -o $i -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -i $i -p tcp --sport 443 -j ACCEPT
 
 
 #MOSTRAR REGLAS
